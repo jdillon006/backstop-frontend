c917b71a139c6dea7a3e7a21fb2630c196703b58
jdillon006 <jeffreydillon@gmail.com>
2026-06-19 12:33:44 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import DashboardPage from './pages/DashboardPage';
 import BriefingPage from './pages/BriefingPage';
